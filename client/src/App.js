94412fd85463921d4766f1f9fba868862f3f7cad
Pamela Mawoyo <p_mawoyo@yahoo.com>
2023-01-31 18:52:02 -0500

    new_videoplayer

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,63 @@
-import {Routes, Route} from 'react-router-dom';
-import HomePage from './views/Home';
-import RoomPage from './views/Room';
-import Header from './components/Header'
-import { PeerProvider } from './providers/Peer';
-import { SocketProvider } from './providers/Socket';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React from 'react';
+import { Typography, AppBar } from "@mui/material";
+import VideoPlayer from './components/VideoPlayer';
+import Sidebar from './components/Sidebar';
+import Notifications from './components/Notifications';
+import { makeStyles } from "tss-react/mui";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App roomPage-container">
+    <div className={classes.wrapper}>
       <span className='bub a'></span>
-            <span className='bub b'></span>
-            <span className='bub c'></span>
-            <span className='bub d'></span>
-            <span className='bub e'></span>
-            <span className='bub f'></span>
-            <span className='bub g'></span>
-            <span className='bub h'></span>
-            <span className='bub i'></span>
-            <span className='bub j'></span>
-      <Header/>
-      <SocketProvider>
-        <PeerProvider>
-          <Routes>
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="/room/:roomId" element={<RoomPage/>}/>
-          </Routes>
-        </PeerProvider>
-      </SocketProvider>
+      <span className='bub b'></span>
+      <span className='bub c'></span>
+      <span className='bub d'></span>
+      <span className='bub e'></span>
+      <span className='bub f'></span>
+      <span className='bub g'></span>
+      <span className='bub h'></span>
+      <span className='bub i'></span>
+      <span className='bub j'></span>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">Header</Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Sidebar>
+        <Notifications />
+      </Sidebar>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">Footer</Typography>
+      </AppBar>
     </div>
   );
-}
+};
 
 export default App;
--- server.js
+++ server.js
@@ -1,58 +1,40 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const {Server} = require('socket.io');
-const http = require('http');
-const cors = require('cors');
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
 
-const app = express();
-const port = 8000;
-const server = http.createServer(app);
-const io = new Server( server, {
-    cors: true
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
 });
 
-// require('./server/config/mongoose.config');
 app.use(cors());
-app.use(express.json());
-// app.use(express.urlencoded({extended:true}))
-// require('./server/routes/project.routes')(app);
-app.use(bodyParser.json());
 
-const emailToSocketMapping = new Map();
-const socketToEmailMapping = new Map();
+const port = 8000;
 
-io.on('connection', (socket)=>{
-    console.log("New Connection");
-    socket.on("join-room", (data)=>{
-        const{roomId, emailId} = data;
-        console.log('User', emailId, 'Joined Room', roomId);
-        emailToSocketMapping.set(emailId, socket.id);
-        socketToEmailMapping.set(socket.id, emailId);
-        socket.join(roomId);
-        socket.emit('joined-room', {roomId});
-        socket.broadcast.to(roomId).emit('user-joined', {emailId});
-    });
+app.get('/', (req, res) => {
+	res.send('Server is running');
+});
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
 
     socket.on("send-message", (data)=>{
         socket.broadcast.emit("receive-message", data);
     });
-
-    socket.on('call-user', data =>{
-        const { emailId, offer} = data;
-        const fromEmail = socketToEmailMapping.get(socket.id);
-        const socketId = emailToSocketMapping.get(emailId);
-        socket.to(socketId).emit('incoming-call', { from: fromEmail, offer})
-    });
-
-    socket.on('call-accepted', data =>{
-        const {emailId, ans} = data;
-        const socketId = emailToSocketMapping.get(emailId);
-        socket.to(socketId). emit('call-accepted', {ans});
-    })
 });
-    
-app.listen(port, () => console.log(`Listening on port: ${port}`) );
-io.listen(8001);
 
-//the following line has been replaced in the package.json file with line 7
-// "test": "echo \"Error: no test specified\" && exit 1"
+server.listen(port, () => console.log(`Server is running on port ${port}`));
